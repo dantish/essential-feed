1d649a909945783f5fc0d57cc83952bbf9a23254
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2021-11-08 19:02:41 +0200

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Daniel Tischenko on 08.11.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
