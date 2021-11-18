6afa8069f2e6252334d2b09c88350ae9c6efa4c8
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2021-11-18 10:55:27 +0200

    Delivers invalidData error on 200 HTTP response with invalid json

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-public typealias HTTPClientResult = Result<HTTPURLResponse, Error>
+public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
 
 public protocol HTTPClient {
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
