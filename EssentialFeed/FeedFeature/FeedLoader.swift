762e7928453d7ea0a87ae39b259bf284204c1837
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2021-11-19 10:42:03 +0200

    Make the `FeedLoader` protocol public

--- EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -6,6 +6,6 @@ import Foundation
 
 public typealias LoadFeedResult = Result<[FeedItem], Error>
 
-protocol FeedLoader {
+public protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)
 }
