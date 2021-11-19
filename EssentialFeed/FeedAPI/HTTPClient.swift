44fb43802afdd4c4096ebdb74eefbfecec1f32be
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2021-11-19 02:50:48 +0200

    Move HTTPClient to its own file

---
+++ EssentialFeed/FeedAPI/HTTPClient.swift
@@ -0,0 +1,11 @@
+//
+//  Created by Daniel Tischenko on 19.11.2021.
+//
+
+import Foundation
+
+public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
+
+public protocol HTTPClient {
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
