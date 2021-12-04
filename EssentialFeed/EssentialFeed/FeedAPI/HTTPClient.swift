1a92e55324447d56250944dfca8ee6f84844fe15
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2021-12-04 13:47:01 +0200

    Move public typealias declarations into the namespace of the corresponding type

--- EssentialFeed/EssentialFeed/FeedAPI/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/HTTPClient.swift
@@ -4,8 +4,8 @@
 
 import Foundation
 
-public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
-
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+    typealias Result = Swift.Result<(Data, HTTPURLResponse), Error>
+
+    func get(from url: URL, completion: @escaping (Result) -> Void)
 }
--- EssentialFeed/EssentialFeed/FeedFeature/FeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedFeature/FeedLoader.swift
@@ -4,8 +4,8 @@
 
 import Foundation
 
-public typealias LoadFeedResult = Result<[FeedItem], Error>
-
 public protocol FeedLoader {
-    func load(completion: @escaping (LoadFeedResult) -> Void)
+    typealias Result = Swift.Result<[FeedItem], Error>
+
+    func load(completion: @escaping (Result) -> Void)
 }
