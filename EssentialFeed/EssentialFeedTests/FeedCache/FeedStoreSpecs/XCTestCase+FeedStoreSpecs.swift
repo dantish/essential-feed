ce0f9fa62f8e1ee3f723f04b153026fa3ef53424
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-01-03 16:49:59 +0200

    Decrease the timeout for insert/delete expectations since now tests are much faster

--- EssentialFeed/EssentialFeedTests/FeedCache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -123,7 +123,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
             insertionError = receivedInsertionError
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 10.0)
+        wait(for: [exp], timeout: 1.0)
         return insertionError
     }
 
@@ -135,7 +135,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
             deletionError = receivedDeletionError
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 10.0)
+        wait(for: [exp], timeout: 1.0)
         return deletionError
     }
 
