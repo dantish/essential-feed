fd7d4bb94c09927fb32390c843a9a24094658d5e
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-01-29 18:11:48 +0200

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Daniel Tischenko on 28.01.2022.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
