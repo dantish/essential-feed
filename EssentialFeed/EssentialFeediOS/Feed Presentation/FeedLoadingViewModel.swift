5764cab1e1c3235df3b86c6761b20c021fc3a708
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-02-02 14:39:03 +0200

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Daniel Tischenko on 02.02.2022.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Daniel Tischenko on 02.02.2022.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
