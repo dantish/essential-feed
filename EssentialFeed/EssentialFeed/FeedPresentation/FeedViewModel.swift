8a52d8e40e55128671b850b158222f5946849f18
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-02-09 11:48:42 +0200

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Daniel Tischenko on 09.02.2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
