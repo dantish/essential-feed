5669c1b8d3c4f902e7cc8b6e7ac347a83962a694
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-02-09 18:59:16 +0200

    Extract system under test creation to a test factory method

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -16,14 +16,20 @@ final class RemoteFeedImageDataLoader {
 class RemoteFeedImageDataLoaderTests: XCTestCase {
 
     func test_init_doesNotPerformAnyURLRequest() {
-        let client = HTTPClientSpy()
-
-        _ = RemoteFeedImageDataLoader(client: client)
+        let (_, client) = makeSUT()
 
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
 
     // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedImageDataLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedImageDataLoader(client: client)
+        trackForMemoryLeaks(client)
+        trackForMemoryLeaks(sut)
+        return (sut, client)
+    }
 
     private class HTTPClientSpy {
         var requestedURLs: [URL] = []
