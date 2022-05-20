1d108021d0946050e8a18f38c1f44b041dcc8d2a
test <test@test.com>
2022-05-20 16:54:17 +0300

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialAppTests/EssentialAppTests.swift
@@ -0,0 +1,11 @@
+//
+//  EssentialAppTests.swift
+//  EssentialAppTests
+//
+//  Created by Daniel Tischenko on 20.05.2022.
+//
+
+import XCTest
+@testable import EssentialApp
+
+class EssentialAppTests: XCTestCase {}
