c939e69d580d50fe7b012def15a12cc23c655d01
test <test@test.com>
2022-06-02 16:41:52 +0300

    Map `ImageComments` into `ImageCommentsViewModels`

--- EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageCommentsPresentation/ImageCommentsPresenterTests.swift
@@ -14,6 +14,40 @@ class ImageCommentsPresenterTests: XCTestCase {
         XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
     }
 
+    func test_map_createsViewModel() {
+        let now = Date()
+
+        let comments = [
+            ImageComment(
+                id: UUID(),
+                message: "a message",
+                createdAt: now.adding(minutes: -55),
+                username: "a username"
+            ),
+            ImageComment(
+                id: UUID(),
+                message: "another message",
+                createdAt: now.adding(days: -1),
+                username: "another username"
+            )
+        ]
+
+        let viewModel = ImageCommentsPresenter.map(comments)
+
+        XCTAssertEqual(viewModel.comments, [
+            ImageCommentViewModel(
+                message: "a message",
+                date: "55 минут назад",
+                username: "a username"
+            ),
+            ImageCommentViewModel(
+                message: "another message",
+                date: "1 день назад",
+                username: "another username"
+            )
+        ])
+    }
+
     // MARK: - Helpers
 
     private func localized(_ key: String, file: StaticString = #filePath, line: UInt = #line) -> String {
